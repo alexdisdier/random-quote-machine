485b6d83a93de0089e36e7a66578cd25a6045dd9
Alexandre Disdier <alexandredisdier@Alexandres-MacBook-Pro.local>
2018-11-25 18:29:49 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+class App extends Component {
+  componentDidMount () {
+        const script = document.createElement("script");
+
+        script.src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+    };
+  render() {
+    return (
+      <div className="App" id="quote-box">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Random Quote machine {/*<code>src/App.js</code> and save to reload.*/}
+          </p>
+          {/* }<a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
