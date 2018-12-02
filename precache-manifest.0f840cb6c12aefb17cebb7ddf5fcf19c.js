1149e81a8307e690b6ff66a551ed6164d21a052e
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2018-12-02 05:41:05 +0100

    Updates

---
+++ precache-manifest.0f840cb6c12aefb17cebb7ddf5fcf19c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8507572375d5b5ea7f9",
+    "url": "/random-quote-machine/static/js/runtime~main.b8507572.js"
+  },
+  {
+    "revision": "e7c71bdc34f1616d3ec2",
+    "url": "/random-quote-machine/static/js/main.e7c71bdc.chunk.js"
+  },
+  {
+    "revision": "8f869ba442b7f5f5b915",
+    "url": "/random-quote-machine/static/js/1.8f869ba4.chunk.js"
+  },
+  {
+    "revision": "e7c71bdc34f1616d3ec2",
+    "url": "/random-quote-machine/static/css/main.b97e53a2.chunk.css"
+  },
+  {
+    "revision": "e9b21c1da6732e71aaaa6218edee6006",
+    "url": "/random-quote-machine/index.html"
+  }
+];
